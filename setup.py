d7051ce18f0ffddd92baa08b5dac8b89f6a0deef
Bruno Freitas <bruno.freitas@intmed.com.br>
2020-02-05 13:11:55 -0300

    Version 1.1.0 setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='django-rest-action-serializer',
-    version='1.0.0',
+    version='1.1.0',
     packages=setuptools.find_packages(),
     description='A Django app that provides a serializer mixin that allows You to customize the fields'
                 'according to the action provided without the need to'
